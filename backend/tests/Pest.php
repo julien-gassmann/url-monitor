308e0d67f299011d4101259dc0fa83ad2ee9f2b4
juliengassmann <juliengassmann974@gmail.com>
2026-01-06 02:03:40 +0100

    ops(backend): add MariaDB container for tests + set up test environment

--- backend/tests/Pest.php
+++ backend/tests/Pest.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Queue;
 use Tests\TestCase;
 
 /*
@@ -16,7 +20,16 @@ use Tests\TestCase;
 
 pest()->extend(TestCase::class)
     ->use(RefreshDatabase::class)
-    ->in('Feature', 'Unit');
+    ->in('Feature', 'Unit')
+    ->beforeEach(function (): void {
+        Bus::fake();
+        Queue::fake();
+        Mail::fake();
+
+        config()->set('app.time_traveller_mode_enabled', false);
+        config()->set('app.keep_access_token_in_cache', false);
+        config()->set('mail.sending_enabled', true);
+    });
 
 /*
 |--------------------------------------------------------------------------
