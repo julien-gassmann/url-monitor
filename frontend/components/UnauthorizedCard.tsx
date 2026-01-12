e576ed4f054b90b297cfd7dffde8f3cbd9c50dbd
juliengassmann <juliengassmann974@gmail.com>
2026-01-12 10:42:50 +0100

    fix(frontend): center unauthorized card

--- frontend/components/UnauthorizedCard.tsx
+++ frontend/components/UnauthorizedCard.tsx
@@ -6,10 +6,10 @@ import { LuShieldAlert } from 'react-icons/lu';
 
 export default function UnauthorizedCard() {
     return (
-        <div className="w-full sm:w-5/6 md:w-3/4 lg:w-2/3 xl:w-1/2 2xl:w-1/3">
+        <div className="w-full flex flex-col items-center justify-center min-h-[calc(100vh-6rem)]">
             <div className="p-6 flex flex-col space-y-6 items-center justify-center bg-white rounded-3xl shadow-lg">
                 <div className="flex items-center gap-2">
-                    <LuShieldAlert className="text-red-400" size={30} />
+                    <LuShieldAlert className="text-red-400" size={30}/>
                     <p className="font-bold">Accès non authorisé.</p>
                 </div>
 
